22616172910f1a3e8570775d619a3a9963c12511
CapHello <antoniocaprio2000@gmail.com>
2026-07-09 15:01:43 +0200

    feat: creata base del form di login
    Signed-off-by: CapHello <antoniocaprio2000@gmail.com>

---
+++ JavaProject/src/main/java/it/unina/prenotazioni/boundary/Swing/loginFrame.java
@@ -0,0 +1,4 @@
+package it.unina.prenotazioni.boundary.Swing;
+
+public class loginFrame {
+}
--- JavaProject/src/main/java/it/unina/prenotazioni/eseguibile/PrenotazioniApplication.java
+++ JavaProject/src/main/java/it/unina/prenotazioni/eseguibile/PrenotazioniApplication.java
@@ -1,4 +1,4 @@
-package it.unina.prenotazioni;
+package it.unina.prenotazioni.eseguibile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
